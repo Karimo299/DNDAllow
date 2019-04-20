7d26b66e97b2137b6c0d03cfd9484d24f8d85ddd
Karimo299 <karimo299@Karims-MacBook-Pro.local>
2019-04-20 03:02:51 -0400

    Update

--- dndallow/DNDARootListController.m
+++ dndallow/DNDARootListController.m
@@ -1,4 +1,6 @@
 #include "DNDARootListController.h"
+#include <spawn.h>
+#include <signal.h>
 
 @implementation DNDARootListController
 
@@ -8,6 +10,30 @@
 	}
 
 	return _specifiers;
+}
+
+- (void) respring {
+	UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Respring"
+	message:@"Are You Sure You Want To Respring?"
+	preferredStyle:UIAlertControllerStyleActionSheet];
+
+	UIAlertAction *respringBtn = [UIAlertAction actionWithTitle:@"Respring"
+	style:UIAlertActionStyleDestructive handler:^(UIAlertAction * action) {
+		pid_t pid;
+		int status;
+		const char* args[] = {"killall", "SpringBoard", NULL};
+		posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char*
+		const*)args, NULL);
+		waitpid(pid, &status, WEXITED);
+	}];
+
+	UIAlertAction *cancelBtn = [UIAlertAction actionWithTitle:@"Cancel"
+	style:UIAlertActionStyleCancel handler:^(UIAlertAction * action) {}];
+
+	[alert addAction:respringBtn];
+	[alert addAction:cancelBtn];
+
+	[self presentViewController:alert animated:YES completion:nil];
 }
 
 - (void) paypal {
